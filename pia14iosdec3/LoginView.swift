2fad0333763a69689af826576f5d067ab6b46322
Bill Mårtensson <bill@magictechnology.se>
2025-12-10 14:06:40 +0100

    pia14

--- pia14iosdec3/LoginView.swift
+++ pia14iosdec3/LoginView.swift
@@ -9,44 +9,106 @@ import SwiftUI
 
 struct LoginView: View {
     
+    enum TheActivateFieldNow {
+        case email, password
+    }
+    
     @State var useremail = ""
     @State var userpassword = ""
     
     @State var logincode = LoginCode()
     
+    @FocusState private var focusedField: TheActivateFieldNow?
+        
     var body: some View {
-        VStack {
+        
+        GeometryReader { geometry in
             
-            if logincode.showError != nil {
+            VStack {
                 VStack {
-                    Text(logincode.showError!)
                 }
-                .frame(width: 300, height: 100)
+                .frame(width: 300, height: 5)
                 .background(Color.red)
-            }
-            
-            
-            TextField("Email", text: $useremail)
-            TextField("Password", text: $userpassword)
+                
+                Text("\(geometry.size.height)")
+                                
+                Spacer()
+                
+                if logincode.showError != nil {
+                    VStack {
+                        Text(logincode.showError!)
+                    }
+                    .frame(width: 300, height: 100)
+                    .background(.red)
+                }
+                
+                VStack {
+                }
+                .frame(width: 300, height: focusedField == nil ? 200 : 100)
+                .background(Color.cyan)
+                
+                Text("Email")
+                    .font(focusedField == .email ? Font.title3.bold() : Font.title3)
+                TextField("Email", text: $useremail)
+                    .border(focusedField == .email ? .black : .clear)
+                    .padding(.horizontal)
+                    .autocapitalization(.none)
+                    .textContentType(.emailAddress)
+                    .autocorrectionDisabled(true)
+                    .submitLabel(.next)
+                    .keyboardType(.emailAddress)
+                    .focused($focusedField, equals: .email)
+                    .onSubmit {
+                        print("EMAIL SUBMIT")
+                        focusedField = .password
+                    }
+                
+                Text("Password")
+                    .font(focusedField == .password ? Font.title3.bold() : Font.title3)
+                SecureField("Password", text: $userpassword)
+                    .border(focusedField == .password ? .black : .clear)
+                    .padding(.horizontal)
+                    .submitLabel(.send)
+                    .keyboardType(.default)
+                    .focused($focusedField, equals: .password)
+                    .onSubmit {
+                        print("PASSWORD SUBMIT")
+                        Task {
+                            await logincode.userlogin(useremail: useremail, userpassword: userpassword)
+                        }
+                    }
 
-            Button("Login") {
-                Task {
-                    await logincode.userlogin(useremail: useremail, userpassword: userpassword)
+                Button("Login") {
+                    Task {
+                        await logincode.userlogin(useremail: useremail, userpassword: userpassword)
+                    }
                 }
+                Button("Register") {
+                    Task {
+                        await logincode.userregister(useremail: useremail, userpassword: userpassword)
+                    }
+                }
+                Button("Forgot password") {
+                    Task {
+                        await logincode.forgotpassword(useremail: useremail)
+                    }
+                }
+                
+                Spacer()
+                
+                VStack {
+                }
+                .frame(width: 300, height: 5)
+                .background(Color.blue)
+                
             }
-            Button("Register") {
-                Task {
-                    await logincode.userregister(useremail: useremail, userpassword: userpassword)
-                }
-            }
-            Button("Forgot password") {
-                Task {
-                    await logincode.forgotpassword(useremail: useremail)
-                }
+            .onTapGesture {
+                print("TAP TAP")
+                focusedField = nil
             }
 
         }
-        
+
     }
 }
 
